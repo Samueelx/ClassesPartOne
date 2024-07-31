58a63e92a225eb3a32393798fbbcfd8225fc78d9
Samueelx <gsammygitau@gmail.com>
2024-07-31 17:48:25 +0300

    Add setter methods.

--- src/Car.java
+++ src/Car.java
@@ -25,6 +25,32 @@ public class Car {
         return doors;
     }
 
+    public void setMake(String make){
+        if(make == null) make = "Unknown";
+        String lowercaseMake = make.toLowerCase();
+        switch (lowercaseMake){
+            case "mercedes-benz", "audi", "subaru", "volkswagen" -> this.make = make;
+            default ->  this.make = "Unsupported";
+        }
+
+    }
+
+    public void setModel(String model){
+        this.model = model;
+    }
+
+    public void setDoors(int doors){
+        this.doors = doors;
+    }
+
+    public void setColour(String colour){
+        this.colour = colour;
+    }
+
+    public void setConvertible(boolean convertible) {
+        isConvertible = convertible;
+    }
+
     public void describeCar(){
         System.out.println(doors + "-Door " +
                 colour + " " + make + " " +
--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,11 @@
 public class Main {
     public static void main(String[] args) {
         Car car = new Car();
+        car.setMake("Mercedes-Benz");
+        car.setModel("S500");
+        car.setColour("Gray");
+        car.setDoors(4);
+        car.setConvertible(true);
         System.out.println("Make = " + car.getMake());
         System.out.println("Model = " + car.getModel());
         car.describeCar();
