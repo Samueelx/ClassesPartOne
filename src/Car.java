55bab72dee4a7425fb6bfd0b201cc266879d8d12
Samueelx <gsammygitau@gmail.com>
2024-07-31 16:25:36 +0300

    Add getter methods.

--- src/Car.java
+++ src/Car.java
@@ -1,7 +1,33 @@
 public class Car {
-    private String make;
-    private String model;
-    private String colour;
-    private boolean isConvertible;
-    private int doors;
+    private String make = "Mercedes-Benz";
+    private String model = "S500";
+    private String colour = "Black";
+    private boolean isConvertible = false;
+    private int doors = 4;
+
+    public String getMake(){
+        return make;
+    }
+
+    public String getModel(){
+        return model;
+    }
+
+    public String getColour(){
+        return colour;
+    }
+
+    public boolean isConvertible() {
+        return isConvertible;
+    }
+
+    public int getDoors() {
+        return doors;
+    }
+
+    public void describeCar(){
+        System.out.println(doors + "-Door " +
+                colour + " " + make + " " +
+                model + " " + (isConvertible ? "Convertible" : ""));
+    }
 }
